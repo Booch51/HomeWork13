70cd095d357a40c8d48b40a06be08aa5799606af
Booch51 <iruintrocrypt@gmail.com>
2024-08-26 11:16:30 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,27 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Author author = new Author("Теодор", "Драйзер");
+        Author author1 = new Author("Айн", "Рэнд");
+        Book book = new Book("Финансист", 1912, author);
+        Book book1 = new Book("Атлант расправил плечи", 1957, author1);
+
+
+        System.out.print(author.getFirstName() + " ");
+        System.out.println(author.getLastName());
+
+        System.out.println("Год публикации - " + book.getYearOfPublication());
+
+        book.setYearOfPublication(1944);
+
+        System.out.println("Год публикации - " + book.getYearOfPublication());
+
+        System.out.println(book1.toString());
+        System.out.println(book.toString());
+
+        System.out.println(book.hashCode());
+
+        System.out.println(author.equals(author1));
     }
 }
